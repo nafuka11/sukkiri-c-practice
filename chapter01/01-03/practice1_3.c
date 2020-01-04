4cac19f8b2af83a4aa0cfa6dcd404d2ad0d6bc91
nafuka <42476527+nafuka11@users.noreply.github.com>
2020-01-04 12:19:41 +0900

    Add chapter01

---
+++ chapter01/01-03/practice1_3.c
@@ -0,0 +1,14 @@
+/**
+ * @file practice1_3.c
+ * @brief 『スッキリわかるC言語入門』練習問題1-3
+ * @author nafuka11
+ * @date 2019-12-26
+ * @details プログラムの開始・終了表示
+ */
+#include <stdio.h>
+int main(void)
+{
+  printf("プログラムを開始します\n");
+  printf("プログラムを終了します\n");
+  return 0;
+}
